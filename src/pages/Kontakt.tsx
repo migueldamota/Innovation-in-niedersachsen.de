ad9d020b86e144ea8b1e39f8724b6f1b7a4de205
migueldamota <miguel@damota.de>
2022-06-17 08:19:10 +0200

    contact: error messages

--- src/pages/Kontakt.tsx
+++ src/pages/Kontakt.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { createRef, FormEvent, useState } from "react";
 import Section, { SectionHeader } from "../components/Section";
 
 export default function Kontakt () {
@@ -6,10 +6,24 @@ export default function Kontakt () {
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
 
+    const [error, setError] = useState({ type: "", message: "" });
+
+    const formRef = createRef<HTMLFormElement>();
+
     async function onSubmit (event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
-        
+        if (!name || !email || !message) {
+            setError({ type: "error", message: "Bitte füllen Sie alle Felder aus" });
+            return;
+        }
+
+        const form = formRef.current;
+        if (!form) return;
+
+        form.reset();
+
+        setError({ type: "success", message: "Ihre Nachricht wurde versendet" });
     }
 
     return (
@@ -20,7 +34,9 @@ export default function Kontakt () {
 
                 <h4 style={{ fontWeight: "normal", textAlign: "center", color: "var(--color-2)" }}>Noch Fragen? Schreibe uns eine Nachricht und wir werden dir so schnell wie möglich antworten!</h4>
 
-                <form onSubmit={onSubmit}>
+                <form onSubmit={onSubmit} ref={formRef}>
+                    { error.message && <div className={"error-" + error.type}>{error.message}</div> }
+
                     <input type="text" name="name" placeholder="Dein Name" value={name} onChange={(e) => setName(e.target.value)} />
                     <input type="email" name="email" placeholder="Deine Email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <textarea placeholder="Deine Nachricht" value={message} onChange={(e) => setMessage(e.target.value)} />
